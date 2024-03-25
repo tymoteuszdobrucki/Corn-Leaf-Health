7b8eb4a711fb0848f4767b57017d000c83ca4cdb
tymoteuszdobrucki <tymoteuszdobrucki.lote@gmail.com>
2024-03-25 22:08:43 +0100

    Added prediction

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from cornLeafHealth import logger
 from cornLeafHealth.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cornLeafHealth.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from cornLeafHealth.pipeline.stage_03_model_training import ModelTrainingPipeline
+from cornLeafHealth.pipeline.stage_04_model_evaluation import ModelEvaluationPipeline
 
 logger.info("testing the logger")
 
@@ -42,3 +43,15 @@ if __name__ == '__main__':
         logger.info(f">> STAGE {STAGE_NAME} completed <<")
     except Exception as e:
         raise e
+       
+    STAGE_NAME = "Model evaluation" 
+    try:
+        logger.info(f">> STAGE {STAGE_NAME} started <<")
+        obj = ModelEvaluationPipeline()
+        obj.main()
+        logger.info(f">> STAGE {STAGE_NAME} completed <<")
+    except Exception as e:
+        raise e
+
+
+
---
+++ src/cornLeafHealth/pipeline/prediction.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 24 23:53:58 2024
+
+@author: Dobrucki
+"""
+
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import os
+
+PREDICTION_DICT = {
+    0: "Blight",
+    1: "Common Rust",
+    2: "Gray Leaf Spot",
+    3: "Healthy"
+    }
+
+class PredictionPipeline:
+    
+    def __init__(self, filename):
+        self.filename = filename
+        
+        
+    def predict(self):
+        
+        model = load_model(os.path.join("artifacts","training","model.h5"))
+        imagename = self.filename
+        test_image = image.load_img(imagename, target_size = (224,224))
+        test_image = image.img_to_array(test_image)
+        test_image = test_image/255 # rescale 1./255
+        test_image = np.expand_dims(test_image, axis=0)
+        result = model.predict(test_image)
+        result_argmax = np.argmax(model.predict(test_image), axis=1)[0]
+        confidence = round(result[0][result_argmax]*100)
+        if result_argmax == 3:
+            return [{ "Prediction" : f"{PREDICTION_DICT[result_argmax]} with {confidence}% confidence"}]   
+        else:
+            return [{ "Prediction" : f"Disease ({PREDICTION_DICT[result_argmax]}) with {confidence}% confidence"}]    
