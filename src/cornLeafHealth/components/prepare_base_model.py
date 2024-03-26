c630738a588079a3243ea883abd2e843396ad018
tymoteuszdobrucki <tymoteuszdobrucki.lote@gmail.com>
2024-03-26 20:29:41 +0100

    Added CNN model

--- src/cornLeafHealth/components/prepare_base_model.py
+++ src/cornLeafHealth/components/prepare_base_model.py
@@ -42,28 +42,29 @@ class PrepareBaseModel:
         full_model = tf.keras.models.Model(inputs=model.input, outputs=prediction)
         
         # alternative model architecture (no transfer learning)
-#         alternative_model = tf.keras.models.Sequential([
-#             tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(224,224,3)),
-#             tf.keras.layers.MaxPooling2D((2, 2)),
-#             tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
-#             tf.keras.layers.MaxPooling2D((2, 2)),
-#             tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
-#             tf.keras.layers.MaxPooling2D((2, 2)),
-#             tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
-#             tf.keras.layers.MaxPooling2D((2, 2)),
-#             tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
-#             tf.keras.layers.MaxPooling2D((2, 2)),
-#             tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
-#             tf.keras.layers.MaxPooling2D((2, 2)),
-#             tf.keras.layers.Flatten(),
-#             tf.keras.layers.Dense(64, activation='relu'),
-#             tf.keras.layers.Dense(classes, activation='softmax'),
+        alternative_model = tf.keras.models.Sequential([
+            tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(224,224,3)),
+            tf.keras.layers.MaxPooling2D((2, 2)),
+            tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
+            tf.keras.layers.MaxPooling2D((2, 2)),
+            tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
+            tf.keras.layers.MaxPooling2D((2, 2)),
+            tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
+            tf.keras.layers.MaxPooling2D((2, 2)),
+            tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
+            tf.keras.layers.MaxPooling2D((2, 2)),
+            tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
+            tf.keras.layers.MaxPooling2D((2, 2)),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(64, activation='relu'),
+            tf.keras.layers.Dense(classes, activation='softmax'),
 
-# ])
-        # full_model = alternative_model
+])
+        full_model = alternative_model
         
         full_model.compile(
-            optimizer=tf.keras.optimizers.SGD(learning_rate=learning_rate),
+            #optimizer=tf.keras.optimizers.SGD(learning_rate=learning_rate),
+            optimizer='adam',
             loss=tf.keras.losses.CategoricalCrossentropy(),
             metrics=["accuracy"]
             )
